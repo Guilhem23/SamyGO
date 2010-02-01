d9f1be5a92504939279f96494f711d3956fd6f22
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-02-01 12:06:29 +0000

    copy original flash fsr,rfs modules as builded one are broken; dont mount drv and exe partitions

--- oe/trunk/oe/packages/initscripts/files/samygo/samsung-setup.sh
+++ oe/trunk/oe/packages/initscripts/files/samygo/samsung-setup.sh
@@ -5,15 +5,18 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 mkdir /dev/sam
 mount -t tmpfs none /dev/sam -o size=1K,mode=1777
 mount -t tmpfs none /dtv -o size=10M,mode=1777
+mount -t tmpfs none /mtd_ram -o size=10M,mode=1777
 
-SWITCH_FLAG0=/mtd_rwarea/PartitionSwitch_0_0
-SWITCH_FLAG1=/mtd_rwarea/PartitionSwitch_1_0
+# copy original fsr and rfs modules
+mount -t squashfs /dev/tbml7 /mtd_boot
+cp /media/hdd/modules/fsr.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
+cp /media/hdd/modules/fsr_stl.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
+cp /media/hdd/modules/rfs.ko /lib/modules/2.6.18_SELP-ARM/kernel/fs/rfs1g/
+umount /mtd_boot
 
 modprobe fsr
 modprobe fsr_stl
 modprobe rfs
-
-mount -t tmpfs none /mtd_ram -o size=10M,mode=1777
 
 mount -t rfs /dev/stl0/14 /mtd_rwarea
 if [ $? != 0 ] ; then
@@ -21,15 +24,18 @@ if [ $? != 0 ] ; then
 	exit 1
 fi
 
-if [ -e $SWITCH_FLAG0 ]; then
-	echo $SWITCH_FLAG0 " is found..."
-	mount -t rfs -r /dev/tbml8 /mtd_exe/
-	mount -t squashfs /dev/tbml9 /mtd_appdata/
-elif [ -e $SWITCH_FLAG1 ]; then
-	echo $SWITCH_FLAG1 " is found..."
-	mount -t rfs -r /dev/tbml10 /mtd_exe/
-	mount -t squashfs /dev/tbml11 /mtd_appdata/
-fi
+#SWITCH_FLAG0=/mtd_rwarea/PartitionSwitch_0_0
+#SWITCH_FLAG1=/mtd_rwarea/PartitionSwitch_1_0
+
+#if [ -e $SWITCH_FLAG0 ]; then
+#	echo $SWITCH_FLAG0 " is found..."
+#	mount -t rfs -r /dev/tbml8 /mtd_exe/
+#	mount -t squashfs /dev/tbml9 /mtd_appdata/
+#elif [ -e $SWITCH_FLAG1 ]; then
+#	echo $SWITCH_FLAG1 " is found..."
+#	mount -t rfs -r /dev/tbml10 /mtd_exe/
+#	mount -t squashfs /dev/tbml11 /mtd_appdata/
+#fi
 
 echo /dtv/core > /proc/sys/kernel/core_pattern
 echo "32" > /proc/sys/kernel/msgmni
