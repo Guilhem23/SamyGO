1885e644947ea3d245e60896adb3b41dc50fbf9a
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-02-01 12:13:26 +0000

    ops

--- oe/trunk/oe/packages/initscripts/files/samygo/samsung-setup.sh
+++ oe/trunk/oe/packages/initscripts/files/samygo/samsung-setup.sh
@@ -9,9 +9,9 @@ mount -t tmpfs none /mtd_ram -o size=10M,mode=1777
 
 # copy original fsr and rfs modules
 mount -t squashfs /dev/tbml7 /mtd_boot
-cp /media/hdd/modules/fsr.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
-cp /media/hdd/modules/fsr_stl.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
-cp /media/hdd/modules/rfs.ko /lib/modules/2.6.18_SELP-ARM/kernel/fs/rfs1g/
+cp /mtd_boot/modules/fsr.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
+cp /mtd_boot/modules/fsr_stl.ko /lib/modules/2.6.18_SELP-ARM/kernel/drivers/fsr/
+cp /mtd_boot/modules/rfs.ko /lib/modules/2.6.18_SELP-ARM/kernel/fs/rfs1g/
 umount /mtd_boot
 
 modprobe fsr
