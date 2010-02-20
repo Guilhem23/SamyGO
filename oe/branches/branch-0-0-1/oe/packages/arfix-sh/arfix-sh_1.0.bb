b21f055c15019ffa91348ff6f0e075bbeec229cd
tom_van <tom_van@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-02-20 13:00:47 +0000

    WiseLink video player aspect ratio fix for T-RBYDEUC added

---
+++ oe/branches/branch-0-0-1/oe/packages/arfix-sh/arfix-sh_1.0.bb
@@ -0,0 +1,69 @@
+DESCRIPTION = "WiseLink video player aspect ratio fix for exeDSP"
+SECTION = "category"
+MAINTAINER = "Tomas Vanek <use@samygo.forum>"
+HOMEPAGE = "http://samygo.sourceforge.net"
+
+PR = "r1"
+
+DEPENDS = "elfpatcher"
+RDEPENDS = "elfpatcher"
+
+SRC_URI = " \
+	file://${PN}.c \
+	file://${LDSCRIPT} \
+"
+
+COMPATIBLE_HOST = "sh4-linux"
+COMPATIBLE_MACHINE = "T-RBYDEUC"
+#COMPATIBLE_MACHINE = "(T-RBYDEUC|T-AMBDFRC)"
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+PACKAGES = "${PN}"
+
+S = ${WORKDIR}
+
+FW_VER = ${@re.sub('\+Samsung', '', bb.data.getVar('PREFERRED_VERSION_samsung-firmware', d, 1))}
+FIRMWARE = ${MACHINE}-${FW_VER}
+
+# T-RBYDEUC exeDSP has symbols stripped, so we need one ld script
+# with right addresses for each firmware version
+LDSCRIPT = ${FIRMWARE}-arfix.ld
+
+PNF = "${FIRMWARE}-arfix_${PV}"
+ELF = "${PNF}.elfpatch"
+
+EXTRA_CFLAGS += "-Os -m4 -Wall -mnomacsave -fshort-wchar -nostartfiles -nostdlib"
+
+do_compile() {
+        ${CC} ${CFLAGS} ${EXTRA_CFLAGS} -o ${ELF} ${PN}.c -Wl,-T${LDSCRIPT},-n
+	${STRIP} --remove-section=.comment ${ELF}
+	chmod -x ${ELF}
+}
+
+do_install() {
+	echo '#! /bin/sh' > arfix
+	echo >> arfix
+	echo 'elfpatcher -p $(pidof exeDSP)' /usr/share/elfpatches/${ELF} >> arfix
+	mkdir -p ${D}/etc/init.d
+	install -m 0755 arfix ${D}/etc/init.d/
+	mkdir -p ${D}/usr/share/elfpatches
+	install -m 0644 ${ELF} ${D}/usr/share/elfpatches/
+	do_simple_tar
+}
+
+DT = ${WORKDIR}/${PNF}
+
+do_simple_tar() {
+	echo '#! /bin/sh' > arfix.sh
+	echo >> arfix.sh
+	echo './elfpatcher -p $(pidof exeDSP)' ${ELF} >> arfix.sh
+	mkdir -p ${DT}
+	install -m 0755 arfix.sh ${DT}/
+	install -m 0755 ${STAGING_DIR}/sh4-linux/bin/elfpatcher ${DT}/
+	install -m 0644 ${ELF} ${DT}/
+	mkdir -p ${DEPLOY_DIR_TAR}
+	/bin/tar -c -v -f ${DEPLOY_DIR_TAR}/${PNF}.tar -C ${WORKDIR} ${PNF}
+	/bin/gzip -f ${DEPLOY_DIR_TAR}/${PNF}.tar
+}
+
+FILES_${PN} = "/etc/init.d /usr/share/elfpatches"
---
+++ oe/branches/branch-0-0-1/oe/packages/arfix-sh/files/arfix-sh.c
@@ -0,0 +1,240 @@
+//   arfix.c
+//
+// patch STMovie_SetDisplaySize() to take video aspect ratio into account
+// for Samsung TV with T-RBYDEUC firmware 
+///////////////////////////////////////////////////////////////////////////////
+
+// use -fshort-wchar option for gcc!
+typedef unsigned short wchar16_t;
+
+typedef struct {
+  void *handle1;
+  void *handle2;
+} t_VideoInfoStruct;
+
+extern t_VideoInfoStruct mm_VideoInfoStruct[];
+extern int mm_VideoInfo;
+extern int videoInputWidth;
+extern int videoInputHeight;
+
+int STVID_GetInputWindowMode(void *handle, int *pMode, void *twoInts);
+int STVID_GetOutputWindowMode(void *handle, int *pMode, void *twoInts);
+
+int STVID_SetOutputWindowMode(void *handle, int mode, void *twoInts);
+int STVID_SetInputWindowMode(void *handle, int mode, void *twoInts);
+
+int STVID_SetSize(void *handle,
+		  int inX, int inY, int inWidth, int inHeight,
+		  int outX, int outY, int outWidth, int outHeight);
+
+int DebugPrintf(const char *format, ...);
+
+#define PROF_FIT_MODE 115
+
+extern void *profileStore;
+int GetProfile(void *pProfileStore, int profileNum, unsigned char *bf,
+	       int size);
+int SetProfile(void *pProfileStore, int profileNum, unsigned char *bf,
+	       int size);
+
+extern void *resourceStore;
+const wchar16_t *GetWString(void *pResourceStore, int resNum);
+
+extern void *applicationStore;
+void *GetApplication(void *pApplicationStore, int i);
+
+struct event_s {
+  int unused;
+  void *pApp144c;
+  int nMenu;
+  int zero1;
+  int zero2;
+};
+int SendEvent(struct event_s *, int r5, int r6);
+
+///////////////////////////////////////////////////////////////////////////////
+
+const struct {
+  int ratio900;			// multiplied by common multiplier 900
+  union {
+    int nStringResource;
+    wchar16_t *wstr;
+  } desc;
+} modeTab[] = {
+// *INDENT-OFF*
+	{		.desc.nStringResource = 70 },	// Auto Fit
+	{		.desc.nStringResource = 71 },	// Original
+	{ 900 * 4 / 3,	.desc.nStringResource = 29 },	// 4:3
+	{ 900 * 16 / 9,	.desc.nStringResource = 25 },	// 16:9
+	{ 9 * 221,	.desc.wstr = L"2.21:1"},
+// *INDENT-ON*
+};
+
+enum {
+  FIT_MODE_FIRST = 1,
+  FIT_MODE_AUTO = FIT_MODE_FIRST,
+  FIT_MODE_ORIG,
+  FIT_MODE_LAST = sizeof(modeTab) / sizeof(modeTab[0])
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+int STMovie_SetDisplaySize(int fit)
+    __attribute__ ((section("patch1STMovie_SetDisplaySize")));
+int STMovie_SetDisplaySize(int fit)
+{
+  void *handle = mm_VideoInfoStruct[mm_VideoInfo].handle1;
+  int iw = videoInputWidth;
+  int ih = videoInputHeight;
+  const int pw = 1920;		// hopefully all pannels are full HD
+  const int ph = 1080;
+  int ow, oh, ox, oy;
+  int w, h;
+  int mode;
+  int po[2];
+  int pi[2];
+
+  if (iw < 16)
+    iw = 16;			// some reasonable minimum, just for the case
+  if (ih < 9)
+    ih = 9;
+  if (iw == 1920 && ih == 1088) {
+    ih = 1080;			// don't scale, just cut
+  }
+
+  ow = pw;
+  oh = ph;
+
+  // not using switch ... case construction because of big overhead on SH4 arch
+
+  if (fit == FIT_MODE_ORIG) {
+
+    // original size
+    if (iw > pw)
+      iw = pw;			// cut if too big
+    if (ih > ph)
+      ih = ph;
+    ow = iw;
+    oh = ih;
+
+  } else {
+
+    if (fit > FIT_MODE_ORIG && fit <= FIT_MODE_LAST) {
+
+      // forced aspect ratio
+      w = modeTab[fit - 1].ratio900;
+      h = 900;
+
+    } else {
+
+      // auto fit keeping aspect ratio
+      w = iw;
+      h = ih;
+
+      if (iw == 720 && ih == 576) {
+	// PAL - never has sqare pixels
+	w = 576 * 4 / 3;
+      }
+    }
+
+    // same as w/h > pw/ph, but vithout division
+    if (w * ph > pw * h) {
+      // wider than screen
+      oh = pw * h / w;
+      // stays ow= pw;
+    } else {
+      // narrower or same
+      ow = ph * w / h;
+      // stays oh= ph;
+    }
+  }
+
+  if (ow > pw)
+    ow = pw;
+  ox = (pw - ow) / 2;
+  if (oh > ph)
+    oh = ph;
+  oy = (ph - oh) / 2;
+
+  if (STVID_GetInputWindowMode(handle, &mode, &pi)) {
+    DebugPrintf("STVID_GetInputWindowMode Error\n");
+  }
+  if (STVID_GetOutputWindowMode(handle, &mode, &po)) {
+    DebugPrintf("STVID_GetOutputWindowMode Error\n");
+  }
+  if (STVID_SetInputWindowMode(handle, 0, &pi)) {
+    DebugPrintf("STVID_SetInputWindowMode Error\n");
+  }
+  if (STVID_SetOutputWindowMode(handle, 0, &po)) {
+    DebugPrintf("STVID_SetOutputWindowMode Error\n");
+  }
+
+  DebugPrintf("Patched SetSize In %dx%d, Out %dx%d-%d-%d\n",
+	      iw, ih, ow, oh, ox, oy);
+  if (STVID_SetSize(handle, 0, 0, iw, ih, ox, oy, ow, oh)) {
+    DebugPrintf("STVID_SetSize Error\n");
+  }
+  return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+const wchar16_t *GetZoomOptionString(void)
+    __attribute__ ((section("patch2GetZoomOptionString")));
+const wchar16_t *GetZoomOptionString(void)
+{
+  unsigned char data = 1;
+
+  GetProfile(&profileStore, PROF_FIT_MODE, &data, 1);
+  data--;
+  if (data >= FIT_MODE_LAST) {
+    data = 0;
+  }
+  if (modeTab[data].desc.nStringResource < 0x400000) {
+    // not an addres, use as resource number
+    return GetWString(&resourceStore, modeTab[data].desc.nStringResource);
+  } else {
+    return modeTab[data].desc.wstr;
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+int ZoomOptionLeftRight(void *unknown, int key)
+    __attribute__ ((section("patch3ZoomOptionLeftRight")));
+int ZoomOptionLeftRight(void *unknown, int key)
+{
+  unsigned char data = 1;
+  void *pApp;
+  struct event_s event;
+
+  if (GetProfile(&profileStore, PROF_FIT_MODE, &data, 1) == 0) {
+    return 0;
+  }
+
+  if (key == 4) {
+    data--;
+    if (data < FIT_MODE_FIRST)
+      data = FIT_MODE_LAST;
+  } else {
+    data++;
+    if (data > FIT_MODE_LAST)
+      data = FIT_MODE_FIRST;
+  }
+
+  if (SetProfile(&profileStore, PROF_FIT_MODE, &data, 1) == 0) {
+    return 0;
+  }
+
+  pApp = GetApplication(&applicationStore, 30);
+  if (pApp)
+    pApp += 0x144c;
+
+  event.pApp144c = pApp;
+  event.nMenu = 0x472;
+  event.zero1 = 0;
+  event.zero2 = 0;
+  SendEvent(&event, 0, 0);
+
+  return 1;
+}
