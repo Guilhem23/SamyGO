cd965129a8a7d8898780d7ce115c721135178824
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-02-24 20:21:07 +0000

    cleanup

--- oe/trunk/oe/packages/samsung/files/samsung-start.sh
+++ oe/trunk/oe/packages/samsung/files/samsung-start.sh
@@ -2,7 +2,7 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
-export LD_LIBRARY_PATH="/lib:/usr/lib:/Java/lib:/mtd_cmmlib/Comp_LIB:/mtd_cmmlib/InfoLink/lib:/mtd_cmmlib/GAME_LIB:/mtd_cmmlib/DRM_LIB"
+export LD_LIBRARY_PATH="/lib:/usr/lib:/Java/lib:/mtd_cmmlib/Comp_LIB:/mtd_cmmlib/InfoLink/lib:/mtd_cmmlib/GAME_LIB:/mtd_cmmlib/DRM_LIB:/mtd_cmmlib/YWidget_LIB"
 
 export MAPLE_DEFAULT_PATH=/mtd_cmmlib/InfoLink/lib
 export MAPLE_MANAGER_WIDGET_PATH=/mtd_down/widgets/manager
@@ -10,7 +10,6 @@ export MAPLE_NORMAL_WIDGET_PATH=/mtd_down/widgets/normal
 export MAPLE_WIDGET_DATA_PATH=/mtd_down
 export MAPLE_WIDGET_INCLUDE_PATH=/mtd_down/widgets/inc
 export MAPLE_PLUGIN_DATA_PATH=/mtd_cmmlib/InfoLink/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mtd_cmmlib/YWidget_LIB
 export KF_SLEEP_READ=-2
 echo 30000 > /mtd_rwarea/DelayValue.txt
 #touch /mtd_rwarea/DoPrintYahoo.txt
