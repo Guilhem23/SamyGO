42dffb7dbeff222395d52409ca1781c628a9d19b
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-04-06 09:57:10 +0000

    separate build dir for CL

--- oe/trunk/setup.sh
+++ oe/trunk/setup.sh
@@ -25,9 +25,9 @@ DL_DIR=${DL_DIR:=$OE_BASE/sources}
 # target platform: Chelsea (Leonid)
 MACHINE=${MACHINE:=sdp83}
 
-mkdir -p  ${OE_BASE}/build/conf
+mkdir -p  ${OE_BASE}/build-${DISTRO}/conf
 
-if [ ! -f ${OE_BASE}/build/conf/local.conf ] || [ ! -f ${OE_BASE}/build/env.source ] || [ "$1" = "--force" ]; then
+if [ ! -f ${OE_BASE}/build-${DISTRO}/conf/local.conf ] || [ ! -f ${OE_BASE}/build-${DISTRO}/env.source ] || [ "$1" = "--force" ]; then
 	echo "DL_DIR = \"${DL_DIR}\"
 OE_BASE = \"${OE_BASE}\"
 BBFILES = \"\${OE_BASE}/oe/packages/*/*.bb \${OE_BASE}/oe/packages/apps-cl/*/*.bb\"
@@ -35,27 +35,28 @@ MACHINE = \"${MACHINE}\"
 TARGET_OS = \"linux-gnueabi\"
 DISTRO = \"${DISTRO}\"
 CACHE = \"${OE_BASE}/cache/oe-cache.\${USER}\"
-ASSUME_PROVIDED += \" gmp-native mpfr-native \"" > ${OE_BASE}/build/conf/local.conf
+ASSUME_PROVIDED += \" gmp-native mpfr-native \"" > ${OE_BASE}/build-${DISTRO}/conf/local.conf
 
 	if [ $OE_NUM_THREADS ] && [ $OE_NUM_THREADS -gt 1 ]; then
-		echo "PARALLEL_MAKE = \"-j $OE_NUM_THREADS\"" >> ${OE_BASE}/build/conf/local.conf
+		echo "PARALLEL_MAKE = \"-j $OE_NUM_THREADS\"" >> ${OE_BASE}/build-${DISTRO}/conf/local.conf
 	else
-		echo "# PARALLEL_MAKE = \"-j 3\"" >> ${OE_BASE}/build/conf/local.conf
+		echo "# PARALLEL_MAKE = \"-j 3\"" >> ${OE_BASE}/build-${DISTRO}/conf/local.conf
 	fi
 
 	echo "OE_BASE=\"${OE_BASE}\"
-export BBPATH=\"\${OE_BASE}/oe/:\${OE_BASE}/bb/:\${OE_BASE}/build/\"
+export BBPATH=\"\${OE_BASE}/oe/:\${OE_BASE}/bb/:\${OE_BASE}/build-${DISTRO}/\"
 if [ ! \`echo \${PATH} | grep \${OE_BASE}/bb/bin\` ]; then
-	export PATH=\${OE_BASE}/bb/bin:\${PATH}:\${OE_BASE}/build/tmp/cross/bin
+	export PATH=\${OE_BASE}/bb/bin:\${PATH}:\${OE_BASE}/build-${DISTRO}/tmp/cross/bin
 fi
 export LD_LIBRARY_PATH=
-export LANG=C" > ${OE_BASE}/build/env.source
+export PYTHONPATH=${OE_BASE}/bb/lib
+export LANG=C" > ${OE_BASE}/build-${DISTRO}/env.source
 
-echo "--- Created ${OE_BASE}/build/conf/local.conf and ${OE_BASE}/build/env.source ---"
+echo "--- Created ${OE_BASE}/build-${DISTRO}/conf/local.conf and ${OE_BASE}/build-${DISTRO}/env.source ---"
 
 fi
 
-cd build && source env.source
+cd build-${DISTRO} && source env.source
 
 echo
 echo "--- SamyGO OE environment setup finished ---"
