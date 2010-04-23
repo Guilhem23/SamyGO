eb259f27e28b1639d6c053e0bbcc3b856664afec
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-04-23 20:46:34 +0000

    typo

--- oe/trunk/setup.sh
+++ oe/trunk/setup.sh
@@ -69,7 +69,7 @@ echo
 if [ "${DISTRO}" = "samygo-cl" ]; then
 echo "--- Usage example: bitbake scummvm-cl ---"
 else
-echo "--- Usage example: bitbake externalboot-CHL7DEUC ---"
+echo "--- Usage example: bitbake externalboot-T-CHL7DEUC ---"
 fi
 echo
 
