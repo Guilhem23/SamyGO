74b706a3ca227e4695f883bf3b2ee757d62f4b20
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-05-16 10:48:40 +0000

    few fixes

--- oe/trunk/oe/recipes/initscripts/files/samygo/startx.sh
+++ oe/trunk/oe/recipes/initscripts/files/samygo/startx.sh
@@ -2,9 +2,10 @@
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+rm /tmp/.X0-lock
 Xvfb :0 -screen 0 1280x760x24 &
-x11vnc &
+x11vnc > /dev/null 2> /dev/null &
+export DISPLAY=127.0.0.1:0
 openbox &
-xterm &
 
 : exit 0
