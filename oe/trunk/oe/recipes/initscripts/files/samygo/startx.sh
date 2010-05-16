2477c86a7bcbd9d35aebc48154b82a9651f25886
aquadran <aquadran@e4d1339e-07e7-4a9d-b57a-aed23070e7d0>
2010-05-16 11:12:39 +0000

    added few more options

--- oe/trunk/oe/recipes/initscripts/files/samygo/startx.sh
+++ oe/trunk/oe/recipes/initscripts/files/samygo/startx.sh
@@ -4,7 +4,7 @@ PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 rm /tmp/.X0-lock
 Xvfb :0 -screen 0 1280x760x24 &
-x11vnc > /dev/null 2> /dev/null &
+x11vnc -24to32 -forever -nopw > /dev/null 2> /dev/null &
 export DISPLAY=127.0.0.1:0
 openbox &
 
